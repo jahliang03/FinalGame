b536447a350c0a05aae1e68caf8e8b31421f184e
jasmine <jahliang@ucsc.edu>
2024-06-12 15:25:33 -0700

    updating instructions

--- src/scenes/titlescreen.js
+++ src/scenes/titlescreen.js
@@ -9,15 +9,15 @@ class TitleScene extends Phaser.Scene {
         // Load the start button image
         this.load.image('startButton', 'assets/clover.png'); // Adjust the path to your image file
         // Load the text 
-        this.load.image('titleText', 'assets/text.png')
-
+        this.load.image('titleText', 'assets/text.png');
+        this.load.image('instructions', 'assets/instructions.png');
         // Load the credits
-        this.load.image('credits', 'assets/credit.png')
+        this.load.image('credits', 'assets/credit.png');
     }
 
     create() {
         // Add a title text button in the center
-        this.title = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY - 80, 'title')
+        this.title = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY - 80, 'title');
         this.title.setScale(0.5);
         // Add a start button in the center
         this.startButton = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY + 40, 'startButton').setInteractive();
@@ -26,13 +26,14 @@ class TitleScene extends Phaser.Scene {
         this.titleText = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY + 150, 'titleText');
         this.titleText.setScale(0.5);
 
+        this.instructions = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY + 180, 'instructions');
+        this.instructions.setScale(0.5);
+
         this.credits = this.add.image(this.cameras.main.centerX - 240, this.cameras.main.centerY + 320, 'credits');
         this.credits.setScale(0.4);
         // Start the game when the start button is clicked
         this.startButton.on('pointerdown', () => {
             this.scene.start('loadScene');
         });
-    
-
     }
 }
